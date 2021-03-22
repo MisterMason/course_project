df5af3c0d67dcd50b075d3ef81a0c02b8d173567
MisterMason <masonsanders31@yahoo.com>
2021-03-22 02:47:34 -0400

    Initial Check-in

---
+++ src/Components/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About Us</h2>
+        <p>We have been a family owned business for over 30 years. We are committed to providing great customer service.  Cell phone service, computer repair, networking, and internet services are just a few of the electronic needs we can assist you with. Thank you to all customers who have and continue to support us throughout the years.</p>
+      </div>
+    );
+  }
+}
+ 
+export default About;
